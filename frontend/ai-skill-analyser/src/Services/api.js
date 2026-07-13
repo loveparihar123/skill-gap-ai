f3863ac9a6b70dac1398837f37bc7281213c56fb
Love Parihar <ronakparihar272@gmail.com>
2026-07-13 16:56:20 +0530

    added vite env variables for production

--- frontend/ai-skill-analyser/src/Services/api.js
+++ frontend/ai-skill-analyser/src/Services/api.js
@@ -2,17 +2,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
 });
-
-// Har request mein token attach karo automatically
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) req.headers.Authorization = `Bearer ${token}`;
-  return req;
-});
-
-// Auth
 export const registerUser = (data) => API.post("/auth/register", data);
 export const loginUser = (data) => API.post("/auth/login", data);
 
