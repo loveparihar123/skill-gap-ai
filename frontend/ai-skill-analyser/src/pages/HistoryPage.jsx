efc0582c0a807688fb456eef6c79e6a687f4f527
Love Parihar <ronakparihar272@gmail.com>
2026-07-14 16:14:56 +0530

    changes file path names

--- frontend/ai-skill-analyser/src/pages/HistoryPage.jsx
+++ frontend/ai-skill-analyser/src/pages/HistoryPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { getHistory } from "src/services/api.js";
+import { getHistory } from "../services/api.js";
 import styles from "./HistoryPage.module.css";
 import axios from "axios";
 
